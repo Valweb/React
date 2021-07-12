92584f33a9b057ea03145ac1bc506bea67c55d3e
Valéria França <valeriaweb@gmail.com>
2021-07-12 13:59:26 -0300

    teste 3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import React from "react";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+      <div>
+        <strong>Olá React</strong>!!!
+      </div>, 
+      document.getElementById("root"));
